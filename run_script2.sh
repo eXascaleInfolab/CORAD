961cd0321c2df79920843b79e2a1396a7cbfae2a
Abdel <abdel@wifi-unifr-129-160.unifr.ch>
2019-08-27 16:44:36 +0200

    results to excel

--- run_script2.sh
+++ run_script2.sh
@@ -1,5 +1,5 @@
 
-python3 main.py 'Gas' '../Datasets/archive_ics/gas-sensor-array-temperature-modulation/20160930_203718.csv' '../Datasets/archive_ics/gas-sensor-array-temperature-modulation/20160930_203718.csv' 15 0.4 4
+python3 main.py 'Gas' '../Datasets/archive_ics/gas-sensor-array-temperature-modulation/20160930_203718-2.csv' '../Datasets/archive_ics/gas-sensor-array-temperature-modulation/20160930_203718-2.csv' 40 0.4 4
 
 
 '../../../Datasets/archive_ics/gas-sensor-array-temperature-modulation/20160930_203718.csv'
