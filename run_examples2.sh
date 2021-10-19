e598273d4a2d1a2596d41af0e621297c81e4720b
Abdelouahab Khelifati <abdel@exascale.info>
2021-10-19 11:30:47 +0200

    presentation

--- run_examples2.sh
+++
@@ -1,5 +0,0 @@
-
-python3 main.py 'Gas' '../Datasets/archive_ics/gas-sensor-array-temperature-modulation/20160930_203718-2.csv' '../Datasets/archive_ics/gas-sensor-array-temperature-modulation/20160930_203718-2.csv' 40 0.4 4
-
-
-'../../../Datasets/archive_ics/gas-sensor-array-temperature-modulation/20160930_203718.csv'
