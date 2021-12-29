525b52263a35dfec18b60473245763513bd3f2e1
Abdelouahab Khelifati <abdel@exascale.info>
2021-12-29 12:01:15 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ sudo apt install software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt update
 sudo apt -y install python3
-pip3 install matplotlib
-pip3 install luminol
-pip3 install sklearn
-pip3 install plotly
+pip install matplotlib
+pip install luminol
+pip install sklearn
+pip install plotly
