2d33b89ea04864b8f263fd324faa687c6225173c
Abdelouahab Khelifati <abdel@exascale.info>
2021-12-29 16:51:00 +0100

    Update corad.py

--- corad.py
+++ corad.py
@@ -200,17 +200,17 @@ if __name__ == "__main__":
     import os
 
     statinfo_TRISTAN = os.stat(
-        "results/compressed/TRISTAN_out_pickle%s.out" % str(int(TIMESTAMP))
+        "results/compressed/TRISTAN_out_pickle.out" 
     )
     statinfo_TRISTAN = statinfo_TRISTAN.st_size
     # dic['size_TRISTAN'] = statinfo.st_size
     statinfo_CORAD = os.stat(
-        "results/compressed/CORAD_out_pickle%s.out" % str(int(TIMESTAMP))
+        "results/compressed/CORAD_out_pickle.out" 
     )
     statinfo_CORAD = statinfo_CORAD.st_size
 
     # dic['size_CORAD'] = statinfo.st_size
-    statinfo = os.stat("results/compressed/originalData%s.out" % str(int(TIMESTAMP)))
+    statinfo = os.stat("results/compressed/originalData.out"
     statinfo = statinfo.st_size
 
     dic["size_original"] = (statinfo, statinfo)
